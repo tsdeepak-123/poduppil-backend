d768ca44585305fd221f740ecedd8708dc4f3778
Deepak ts <tsdeepak589@gmail.com>
2023-12-10 12:54:03 +0530

    link added

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json())
 
 app.use(cors({
-    origin:["http://localhost:3000"],
+    origin:["https://poduppilconstructions.netlify.app/"],
     methods:["GET","POST","PATCH"],
     credentials:true
 }))

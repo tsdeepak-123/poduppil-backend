35033feae5e12ca050ef317968a2ba6ec47a007f
Deepak ts <tsdeepak589@gmail.com>
2023-12-28 16:46:17 +0530

    frontend env added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require('express')
 const app=express()
 const path=require('path')
 const cors=require('cors')
+require("dotenv").config()
 const userRoute=require('./Routes/user')
 const adminRoute = require('./Routes/Admin')
 
@@ -14,7 +15,7 @@ app.use(express.json())
 
 
 app.use(cors({
-    origin: ["https://poduppilconstructions.com"],
+    origin: [process.env.FrontendLink],
       methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],

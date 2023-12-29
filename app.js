52b40855dacf6b64ac0f53f50f9adf621d8090ff
Deepak ts <tsdeepak589@gmail.com>
2023-12-29 10:28:51 +0530

    cors removed

--- app.js
+++ app.js
@@ -14,12 +14,24 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json())
 
 
-app.use(cors({
-  origin: ["https://www.poduppilconstructions.com"],
-  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+// app.use(cors({
+//   origin: ["https://www.poduppilconstructions.com"],
+//   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+//   credentials: true,
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+// }));
+
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://www.poduppilconstructions.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 
 app.use('/',userRoute)
